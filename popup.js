03f28de82be5f8c7939d20c2feda9a2859865d8a
Oussama <oussamalembarki79@gmail.com>
2026-07-22 16:28:55 +0100

    Clean popup logic

--- popup.js
+++ popup.js
@@ -1,15 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const btn = document.getElementById('toggle');
+    const toggle = document.getElementById('toggle');
     const status = document.getElementById('status');
 
-    chrome.storage.local.get(['enabled'], (res) => {
-        btn.checked = res.enabled || false;
-        status.innerText = btn.checked ? "FOCUS MODE: ACTIVE" : "FOCUS MODE: OFF";
+
+    chrome.storage.local.get(['enabled'], (result) => {
+        toggle.checked = result.enabled || false;
+        updateStatus(toggle.checked);
     });
 
-    btn.onchange = () => {
-        chrome.storage.local.set({ enabled: btn.checked }, () => {
-            status.innerText = btn.checked ? "FOCUS MODE: ACTIVE" : "FOCUS MODE: OFF";
+
+    toggle.addEventListener('change', () => {
+        chrome.storage.local.set({
+            enabled: toggle.checked
         });
-    };
+
+        updateStatus(toggle.checked);
+    });
+
+
+    function updateStatus(enabled) {
+        status.textContent = enabled
+            ? 'FOCUS MODE: ACTIVE'
+            : 'FOCUS MODE: OFF';
+    }
 });
